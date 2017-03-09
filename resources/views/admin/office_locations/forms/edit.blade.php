87ec00ca9fface3fe968e6e36bb3289bf5d56f08
Muhammad Idrees <medriis@gmail.com>
2017-03-09 20:08:44 +0500

    fixed colums width

--- resources/views/admin/office_locations/forms/edit.blade.php
+++ resources/views/admin/office_locations/forms/edit.blade.php
@@ -82,7 +82,7 @@
                 </div>
             </div>
             <div class="row">
-            <div class="col-md-4 col-md-offset-2">
+            <div class="col-md-5 col-md-offset-1">
                 <div class="form-group">
                     <label for="" class="col-sm-2 control-label"></label>
                     <div class="col-sm-10">
