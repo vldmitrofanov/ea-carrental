96d9c9d7defb23efc8df8a7d434e74470de1b30c
root <root@vmi90721.contaboserver.net>
2017-04-29 13:59:31 +0200

    working on serach

--- app/RentalCar.php
+++ app/RentalCar.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Setting;
+use Session;
+use Carbon\Carbon;
 
 class RentalCar extends Model
 {
@@ -31,4 +34,41 @@ class RentalCar extends Model
             return 'administration/dist/img/no_img.png';
         }
     }
+    
+    private function _calculateDateDiff($start , $end){
+        $from = Carbon::parse($start);
+        $to = Carbon::parse($end);
+        $datetime1 = new \DateTime($to); // Today's Date/Time
+        $datetime2 = new \DateTime($from);
+        $interval = $datetime1->diff($datetime2);
+        $data['days'] = $interval->format('%D');
+        $data['hours'] = $interval->format('%H');
+        return $data;
+    }
+    
+    public function getCarPrice(){        
+        $cartData = Session::get('oCart');
+        
+        $o_new_day_per_day =0 ;
+        $oSetting = Setting::where('key', 'currency')->first();
+        $currency = ($oSetting)?$oSetting->value:'USD';
+
+        $time = $this->_calculateDateDiff($cartData->date_from, $cartData->date_to);
+
+        $oRentalCar = $this;
+        print_r($oRentalCar);exit;
+        
+        $oCarType = Types::where('id',$request->input('car_type_id'))->first();
+        if(!$oCarType){
+            return $this->_failedJsonResponse([['Car Type is not valid or has been removed.']]);
+        }
+
+        $oCarModel = CarModel::where('id',$request->input('models'))->first();
+        if(!$oCarModel){
+            return $this->_failedJsonResponse([['Car Model is not valid or has been removed.']]);
+        }
+
+        
+    }
+    
 }
